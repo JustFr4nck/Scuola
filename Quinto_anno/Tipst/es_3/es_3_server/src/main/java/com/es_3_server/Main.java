641aacdca3e4d4e8cab34156c89c9cf49a667c87
lai2-24 <lai2-24@itismeucci.com>
2025-10-07 13:20:01 +0200

    es_3_server

--- Quinto_anno/Tipst/es_3/es_3_server/src/main/java/com/es_3_server/Main.java
+++ Quinto_anno/Tipst/es_3/es_3_server/src/main/java/com/es_3_server/Main.java
@@ -1,28 +1,46 @@
 package com.es_3_server;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+//Server
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("ServerSoket listening in localhost:3000");
-        ServerSocket ss = new ServerSocket(3000);
-        Socket mySocket = ss.accept();  //in ascolto aspettando collegamento di qualcuno
-        System.out.println("Someone connected");
+        System.out.println("Server socket in ascolto sulla porta 3000");
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
-        PrintWriter out = new PrintWriter(mySocket.getOutputStream(), true);
-        String message = in.readLine();
-        System.out.println("Message receaved: " + message);
+        ServerSocket ss = new ServerSocket(3000);   //apro server socket sulla porta 3000
+        Socket myS = ss.accept();                        //crea un socket in ascolta da server, accept blocca, finche qualcuno non si collega alla porta 3000
+        System.out.println("Qualuno si è collegato");
 
-        out.println("Welcome to server V3.1");
+        BufferedReader in = new BufferedReader(new InputStreamReader(myS.getInputStream()));
+        PrintWriter out = new PrintWriter(myS.getOutputStream(), true);
+        
+        
+        out.println("Welcome to server v2.1");
 
-        String fraseRicevuta = in.readLine().toUpperCase();
-        out.println(fraseRicevuta);
-        ss.close(); 
+        boolean check = true;
+        do {
+            String fraseReceived = in.readLine();
+            if (fraseReceived == null) {
+                check = false;
+                break;
+            }
+            
+            fraseReceived = fraseReceived.toUpperCase();
+        
+            if(fraseReceived.equals("!")){
+                check = false;
+                break;
+            }
+            out.println(fraseReceived);
+        
+        } while (check);
+        
+        System.out.println("Communicazione terminata");
+        
+        ss.close();                                      //chiudo la porta
+        
     }
 }
