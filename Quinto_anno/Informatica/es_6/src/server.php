a5458a311d7c8dd67bc235b5628bab8ba9af18a2
JustFr4nck <francyperry07@gmail.com>
2025-10-27 10:05:17 +0100

    es_6_informatica_updated

--- Quinto_anno/Informatica/es_6/src/server.php
+++ Quinto_anno/Informatica/es_6/src/server.php
@@ -1,40 +1,43 @@
-<?php 
-    
+<?php
 
-    function sanitize_input($data) {
+
+function sanitize_input($data)
+{
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
 }
-    
-    $nome = sanitize_input($_GET["name"]);
-    $email = sanitize_input($_GET["email"]);
-    $message = sanitize_input($_GET["message"]);
 
-    $error = false;
+$nome = sanitize_input($_POST["name"]);
+$email = sanitize_input($_POST["email"]);
+$message = sanitize_input($_POST["message"]);
 
-    if (!preg_match("/^[a-zA-ZÀ-ÿ ']+$/u", $nome)) {
-        echo "ERRORE: NOME NON VALIDO<br>";
-        $error = true;
-    }
+$error = false;
+
+if (!preg_match("/^[a-zA-ZÀ-ÿ ']+$/u", $nome)) {
+    echo "ERRORE: NOME NON VALIDO<br>";
+    $error = true;
+}
 
 
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "ERRORE: EMAIL INVALIDA<br>";
-        $error = true;
-    }
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "ERRORE: EMAIL INVALIDA<br>";
+    $error = true;
+}
 
-    if (strlen($message) > 300) {
-        echo "ERRORE: MESSAGGIO TROPPO LUNGO<br>";
-        $error = true;
-    }
+if (strlen($message) > 300) {
+    echo "ERRORE: MESSAGGIO TROPPO LUNGO<br>";
+    $error = true;
+}
 
-    if (!$error) {
-        echo "<strong>Dati ricevuti correttamente!</strong><br>";
-        echo "Nome: $nome<br>";
-        echo "Email: $email<br>";
-        echo "Messaggio: $message<br>";
-    }
+if (!$error) {
+    echo "<strong>Dati ricevuti correttamente!</strong><br>";
+    echo "Nome: $nome<br>";
+    echo "Email: $email<br>";
+    echo "Messaggio: $message<br>";
+}
+
+header("Location: thankyou.php");
 
 ?>
---
+++ Quinto_anno/Informatica/es_6/src/thankyou.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ThankYou</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <div class="bg-gradient-to-br from-sky-100 to-blue-200 min-h-screen flex items-center justify-center">
+        <div class="bg-white shadow-2xl rounded-2xl p-20 w-full max-w-md space-y-6 border border-gray-200 text-center">
+            <h1 class="text-orange-500 font-extrabold text-2xl">Thank YOUUUUUU!!!!</h1>
+        </div>
+    </div>
+</body>
+
+</html>
